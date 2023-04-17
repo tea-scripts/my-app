0c14a06a973ce59d907ce03ea718604fdf29f7e1
teamie <timmieprince@gmail.com>
2023-04-17 17:23:42 +0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AllUsers from './components/Users';
+
+function App() {
+  return (
+    <div>
+      <AllUsers />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Users.jsx
@@ -0,0 +1,23 @@
+import { IconAccessible } from '@tabler/icons-react';
+
+const allUsers = ['John', 'Paul', 'Gab'];
+
+const Users = () => {
+  return (
+    <div className="my-users" id="my-users-id">
+      <ul>
+        {allUsers.map((user) => {
+          const lastName = 'ShakeAndBake';
+          return (
+            <li className="list-item" key={user}>
+              {user} {lastName}
+              <span>{<IconAccessible />}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
